129dfbd0c0dca270841cdc1e32d78fb401944947
rickroII <omnioid11@gmail.com>
2016-11-22 09:11:09 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -30,8 +30,24 @@ public class Main{
     }
 	
 	private static void gameRules(){
-		;
+		System.out.println("------------------------------");
+		System.out.println("Tic Tac Toe Game Rules:");
+		System.out.println("In order to win, you must connect 3");
+		System.out.println("of x's or o's vertically, ");
+		System.out.println("horizontally, or diagonally. You, the user,");
+		System.out.println("are given the choice of 'X' ");
+		System.out.println("or 'O' for your your symbol. ");
+		System.out.println("If you pick an invalid column,");
+		System.out.println("(one that is full or non-existent),");
+		System.out.println("your will be asked to pick a new column.");
+		System.out.println("Lastly, the program will clear itself");
+		System.out.println("as you continue throughout the game");
+		System.out.println("so that your game space does not");
+		System.out.println("become over crowded.");
+		System.out.println("That's all, enjoy the game!");
+		System.out.println("------------------------------");
 	}
+	
 	private static void userInput(){
 		;
 	}
