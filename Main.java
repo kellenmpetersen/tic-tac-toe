c406f32fea6831ca4e2a87a3422687d4e1cde225
rickroII <omnioid11@gmail.com>
2016-11-22 09:04:44 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+/* 
+
+   name: TicTacToe
+   author: Pseupier Coders
+   date: 11/22/16
+   description: a simple game of tic-tac-toe
+
+*/
+
+public class Main{
+	
+	final static int WIDTH = 3;
+	final static int HEIGHT = 3;
+	
+	static char[][]board = new char[WIDTH][HEIGHT];
+	
+	// the main method is the entry point for a program
+    // inside methods the statements get executed in the order they appear in the code
+	public static void main(String[] args){
+		//while (booleanexpression) {
+			gameRules();
+			userInput();
+			gameLogic();
+			checkWin();
+			gameAI();
+			checkWin();
+			clearBoard();
+			displayBoard();
+		//}
+    }
+	
+	private static void gameRules(){
+		;
+	}
+	private static void userInput(){
+		;
+	}
+	private static void gameLogic(){
+		;
+	}
+	private static void checkWin(){
+		;
+	}
+	private static void gameAI(){
+		;
+	}
+	private static void clearBoard(){
+		;
+	}
+	private static void displayBoard(){
+		;
+	}
+}
