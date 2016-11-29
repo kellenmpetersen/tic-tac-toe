93bdd1beacdcdee1e28b6c4208092b83b87bc624
jackdelamotte <jack.delamotte@wchsstudent.org>
2016-11-28 20:52:02 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,8 @@ public class Main{
 	public static void main(String[] args){
 		//while (booleanexpression) {
 			gameRules();
-			userInput();
+			userInput1();
+			userInput2();
 			gameLogic();
 			checkWin();
 			gameAI();
@@ -50,25 +51,6 @@ public class Main{
 		System.out.println("------------------------------");
 	}
 	
-	private static void gameRules(){
-		System.out.println("------------------------------");
-		System.out.println("Tic Tac Toe Game Rules:");
-		System.out.println("In order to win, you must connect 3");
-		System.out.println("of x's or o's vertically, ");
-		System.out.println("horizontally, or diagonally. You, the user,");
-		System.out.println("are given the choice of 'X' ");
-		System.out.println("or 'O' for your your symbol. ");
-		System.out.println("If you pick an invalid column,");
-		System.out.println("(one that is full or non-existent),");
-		System.out.println("your will be asked to pick a new column.");
-		System.out.println("Lastly, the program will clear itself");
-		System.out.println("as you continue throughout the game");
-		System.out.println("so that your game space does not");
-		System.out.println("become over crowded.");
-		System.out.println("That's all, enjoy the game!");
-		System.out.println("------------------------------");
-	}
-	/////////////////
 	public static boolean userInput1()
 	{
 		String a; 
@@ -83,44 +65,51 @@ public class Main{
 				return true;
 			}
 			else if(a.equals("no")) {
-				System.out.println("the program will wait until you are ready to begin.");
+				System.out.println("enter 'yes' when you are ready to begin.");
 				a = input.nextLine();
 			}
 			else {
 				System.out.println("that didn't work try entering 'yes' or 'no' ");
 			}
 		}
+		return true; 
 	}
 	
-	/*public static void userInput2() //method for inputting column and row for token placements
+	public static void userInput2() //method for inputting column and row for token placements
 	{
 		int a;
 		int b;
-		final static int ROW;
-		final static int COLUMN;
+		int column;
+		int row;
+		
 		System.out.println("enter a column in which you would like to place your location (1-3)");
 		Scanner input = new Scanner(System.in);
+		
 		a = input.nextInt();
-		if (a == 1)
-		{
-			COLUMN == 1;
-		}
 		
-		if (a == 2)
+		while(flag == false)
 		{
-			COLUMN == 2;
-		}
+			if (a == 1)
+			{
+				column = 1;
+			}
 		
-		if (a == 3)
-		{
-			COLUMN == 3;
-		} else  {
+			if (a == 2)
+			{
+				column = 2;
+			}
+		
+			if (a == 3)
+			{
+				column = 3;
+			} else  {
 					System.out.println("that's not a vaild column, try entering a number 1-3");
+					a = input.nextInt();
 					return;
 				}
-		
-		
-	}*/
+		}
+	}
+	
 	
 	private static void gameLogic(){
 		;
